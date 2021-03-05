6a747fe80693d1f8feb2f624b0921f813527c4d5
biagavirete <biagavirete@gmail.com>
2021-03-05 14:32:41 -0300

    feat: add accessible references

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,49 +6,58 @@ import { Form } from './styles';
 
 const FormComponent = () => {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data: any) => {
+  const onSubmit = async (data: any) => {
     toast.success('Cadastro realizado')
+
     console.log(data);
   }
 
   const married = watch("married");
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form>
       <h3>Cadastro</h3>
-      <TextField id="standard-basic" label="Nome" name="nome" inputRef={register({ required: true })} />
-      {errors.nome && <p>Campo obrigatório</p>}
-      <TextField id="standard-basic" label="Cidade" name="cidade" inputRef={register} />
-      <TextField id="standard-basic" label="E-mail" type="email" name="email" inputRef={register({
-        required: true,
-        pattern: {
-          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-          message: "E-mail inválido"
-        }
-      })} />
-      {errors.email?.type === "required" && <p>Campo obrigatório</p>}
-      {errors.email?.type === "pattern" && <p>E-mail inválido</p>}
+      <form data-testid="form" onSubmit={handleSubmit(onSubmit)}>
+        <TextField id="standard-basic" label="Nome" name="name" inputRef={register({ required: 'Campo obrigatório' })} />
+        {errors.name && <p role="alert" data-testid="name-error">{errors.name.message}</p>}
+        <TextField id="standard-basic" label="Cidade" name="city" inputRef={register} />
+        <TextField id="standard-basic" label="E-mail" type="email" name="email" inputRef={register({
+          required: 'Campo obrigatório',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "E-mail inválido"
+          }
+        })} />
+        {errors.email && <p role="alert" data-testid="email-error">{errors.email.message}</p>}
 
+        <TextField id="standard-basic" label="Idade" type="number" name="age" inputRef={register({
+          required: 'Campo obrigatório',
+          min: {
+            value: 18,
+            message: "Necessário ser maior de 18 anos"
+          }
+        })} />
 
-      <TextField id="standard-basic" label="Idade" type="number" name="idade" inputRef={register({ required: true, min: 18 })} />
-      {errors.idade?.type === "required" && <p>Campo obrigatório</p>}
-      {errors.idade?.type === "min" && <p>Necessário ser maior de 18 anos.</p>}
+        {errors.age && <p role="alert" data-testid="age-error">{errors.age.message}</p>}
 
-      <label className="married-label">Casado(a)?</label>
-      <div>
-        <input type="radio" name="married" value="true" ref={register({ required: true })} />sim
-          <input type="radio" name="married" value="false" ref={register({ required: true })} />não
+        <label className="married-label">Casado(a)?</label>
+        <div>
+          <label>
+            <input type="radio" name="married" value="true" ref={register({ required: true })} />Sim</label>
+          <label>
+            <input type="radio" name="married" value="false" ref={register({ required: true })} />Não</label>
           {married === 'true' && (
-          <div>
-            <TextField id="standard-basic" label="Nome do cônjuge" type="text" name="conjuge" inputRef={register} />
-          </div>
-        )}
-        {errors.married && <p>Campo obrigatório</p>}
-      </div>
-      <Toaster />
-      <Button type="submit" variant="outlined" color="primary">
-        Enviar
+            <div>
+              <TextField id="standard-basic" label="Nome do cônjuge" type="text" name="conjuge" inputRef={register} />
+            </div>
+          )}
+          {errors.married && <p role="alert" data-testid="married-error">Campo obrigatório</p>}
+        </div>
+        <Toaster />
+        <Button type="submit" role="button" variant="outlined" color="primary">
+          Enviar
       </Button>
+      </form>
     </Form>
   );
 }
